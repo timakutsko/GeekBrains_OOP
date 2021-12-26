f1440dbce6b328ab9b1a6fd2f331b4692c49a39e
Tsimafei <tima.kutsko@gmail.com>
2021-12-26 19:42:19 +0300

    Часть 1

---
+++ Lesson2/Lesson2/BankAccaunt.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson2
+{
+    enum AccauntType
+    {
+        Budget,
+        ForeignCurrency,
+        Savings,
+    }
+    
+    class BankAccaunt
+    {
+        private uint _id;
+        private int _balance;
+        private AccauntType _type;
+
+        public uint Id
+        {
+            get
+            {
+                return _id;
+            }
+            set
+            {
+                string strId = value.ToString();
+                if(strId.Length < 8 || strId.Length > 8)
+                {
+                    _id = 0;
+                    Console.WriteLine("Номер счета содержит 8 символов!");
+                }
+                _id = value;
+            }
+        }
+
+        public int Balance
+        {
+            get
+            {
+                return _balance;
+            }
+            set
+            {
+                _balance = value;
+            }
+        }
+
+        public AccauntType AccauntType
+        {
+            get
+            {
+                return _type;
+            }
+            set
+            {
+                _type = value;
+            }
+        }
+
+        public void PrintAccaunt()
+        {
+            Console.WriteLine($"Id счета: {_id}");
+            Console.WriteLine($"Баланс счета: {_balance}");
+            Console.WriteLine($"Тип счета: {_type}");
+        }
+
+        public void GenerateId()
+        {
+            Random rand = new Random();
+            uint autoId = (uint)rand.Next(10000000, 99999999);
+            Id = autoId;
+        }
+
+    }
+}
---
+++ Lesson2/Lesson2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lesson2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccaunt bAcc_1 = new BankAccaunt();
+            bAcc_1.Id = 12345678;
+            bAcc_1.Balance = 155;
+            bAcc_1.AccauntType = AccauntType.Budget;
+            bAcc_1.PrintAccaunt();
+
+            BankAccaunt bAcc_2 = new BankAccaunt();
+            bAcc_2.GenerateId();
+            bAcc_2.Balance = 100;
+            bAcc_2.AccauntType = AccauntType.Savings;
+            bAcc_2.PrintAccaunt();
+            
+        }
+    }
+}
