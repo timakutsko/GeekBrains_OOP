ca836e408d9d7ade9269e795abfe54912d6a4032
Tsimafei <tima.kutsko@gmail.com>
2021-12-26 22:30:16 +0300

    Часть2
    Реверс

---
+++ Lesson3/Lesson3_String/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson3_String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введи строку:");
+            char[] chArr = Console.ReadLine().ToCharArray();
+            string revStr = string.Empty;
+            for(int i = chArr.Length - 1; i >= 0; i--)
+            {
+                revStr += string.Concat(chArr[i]);
+            }
+            Console.WriteLine($"Реверс: {revStr}");
+        }
+    }
+}
