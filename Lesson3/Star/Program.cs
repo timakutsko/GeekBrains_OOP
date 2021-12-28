b6f823874217b75e7297db3bfc7d686e50f62c64
Tsimafei <tima.kutsko@gmail.com>
2021-12-28 23:02:28 +0300

    *Часть3

---
+++ Lesson3/Star/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson3_Star
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"emails.txt";
+            string newPath = @"justEmails.txt";
+            StreamReader sr = new StreamReader(path);
+            StreamWriter sw = new StreamWriter(newPath);
+
+            while (sr.Peek() != -1)
+            {
+                string line = sr.ReadLine();
+                SearchMail(ref line);
+                sw.WriteLine(line);
+            }
+            sr.Close();
+            sw.Close();
+        }
+
+        public static void SearchMail(ref string s)
+        {
+            s = s.Split('&')[1].Trim();
+        }
+
+    }
+}
