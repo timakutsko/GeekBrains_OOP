b99a06a58ccfb4a94f0a0bb55ccbacd1b60ee732
Tsimafei <tima.kutsko@gmail.com>
2022-01-16 23:12:50 +0300

    Часть1
    Со * - не успел.

---
+++ Lesson5/Lesson5/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Lesson5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RationalNumber number1 = new RationalNumber(2, 5);
+            RationalNumber number2 = new RationalNumber(1, 3);
+            RationalNumber number3 = new RationalNumber(2, 6);
+            int number4 = 5;
+            RationalNumber number5 = new RationalNumber(1, 3);
+
+            bool eq12 = number1 == number2;
+            bool eq13 = number1 == number3;
+            bool eq23 = number2 == number3;
+
+            bool nEq12 = number1 != number2;
+            bool nEq13 = number1 != number3;
+            bool nEq23 = number2 != number3;
+
+            bool ovEq12 = number1.Equals(number2);
+            bool ovEq13 = number1.Equals(number3);
+            bool ovEq14 = number1.Equals(number4);
+
+            bool bigger12 = number1 > number2;
+            bool bigger21 = number2 > number1;
+            bool bigger13 = number1 > number3;
+
+            bool smaller12 = number1 < number2;
+            bool smaller13 = number1 < number3;
+
+            bool smallerOrEq12 = number1 <= number2;
+            bool smallerOrEq35 = number3 <= number5;
+
+            RationalNumber number12 = number1 + number2;
+            RationalNumber number13 = number1 + number3;
+
+            number12++;
+            number13--;
+
+            RationalNumber number1m2 = number1 * number2;
+            RationalNumber number1d2 = number1 / number2;
+
+            float floatNumb = (float)number1d2;
+            int intNumb = (int)number1d2;
+
+            Console.WriteLine(floatNumb.ToString());
+            Console.WriteLine(intNumb.ToString());
+            Console.WriteLine(number12.ToString());
+            Console.WriteLine(smallerOrEq12.ToString());
+            Console.WriteLine(number1m2.ToString());
+        }
+    }
+}
---
+++ Lesson5/Lesson5/RationalNumber.cs
@@ -0,0 +1,220 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson5
+{
+    
+    class RationalNumber
+    {
+        private int _numerator;
+        private int _denominator;
+
+        public RationalNumber() : this (0, 1)
+        {
+        }
+        
+        public RationalNumber (int numer, int denom)
+        {
+            _numerator = numer;
+            _denominator = denom;
+        }
+        
+        public static bool operator ==(RationalNumber n1, RationalNumber n2)
+        {
+            return (n1.Denominator == n2.Denominator && n1.Numerator == n2.Numerator);
+        }
+
+        public static bool operator !=(RationalNumber n1, RationalNumber n2)
+        {
+            return (n1.Denominator != n2.Denominator || n1.Numerator != n2.Numerator);
+        }
+
+        public static bool operator <(RationalNumber n1, RationalNumber n2)
+        {
+            if(n1.Denominator == n2.Denominator)
+            {
+                return (n1.Numerator < n2.Numerator);
+            }
+            else
+            {
+                int overallDenominator = n1.Denominator * n2.Denominator;
+                int temp1 = overallDenominator / n1.Denominator * n1.Numerator;
+                int temp2 = overallDenominator / n2.Denominator * n2.Numerator;
+                return (temp1 < temp2);
+            }
+        }
+
+        public static bool operator >(RationalNumber n1, RationalNumber n2)
+        {
+            if (n1.Denominator == n2.Denominator)
+            {
+                return (n1.Numerator > n2.Numerator);
+            }
+            else
+            {
+                int overallDenominator = n1.Denominator * n2.Denominator;
+                int temp1 = overallDenominator / n1.Denominator * n1.Numerator;
+                int temp2 = overallDenominator / n2.Denominator * n2.Numerator;
+                return (temp1 > temp2);
+            }
+        }
+
+        public static bool operator <=(RationalNumber n1, RationalNumber n2)
+        {
+            if (n1.Denominator == n2.Denominator)
+            {
+                return (n1.Numerator <= n2.Numerator);
+            }
+            else
+            {
+                int overallDenominator = n1.Denominator * n2.Denominator;
+                int temp1 = overallDenominator / n1.Denominator * n1.Numerator;
+                int temp2 = overallDenominator / n2.Denominator * n2.Numerator;
+                return (temp1 <= temp2);
+            }
+        }
+
+        public static bool operator >=(RationalNumber n1, RationalNumber n2)
+        {
+            if (n1.Denominator == n2.Denominator)
+            {
+                return (n1.Numerator >= n2.Numerator);
+            }
+            else
+            {
+                int overallDenominator = n1.Denominator * n2.Denominator;
+                int temp1 = overallDenominator / n1.Denominator * n1.Numerator;
+                int temp2 = overallDenominator / n2.Denominator * n2.Numerator;
+                return (temp1 >= temp2);
+            }
+        }
+
+        public static RationalNumber operator +(RationalNumber n1, RationalNumber n2)
+        {
+            RationalNumber res = new RationalNumber();
+            if (n1.Denominator == n2.Denominator)
+            {
+                res.Numerator = n1.Numerator + n2.Numerator;
+                res.Denominator = n1.Denominator;
+                return res;
+            }
+            else
+            {
+                int overallDenominator = n1.Denominator * n2.Denominator;
+                int temp1 = overallDenominator / n1.Denominator * n1.Numerator;
+                int temp2 = overallDenominator / n2.Denominator * n2.Numerator;
+                res.Numerator = temp1 + temp2;
+                res.Denominator = overallDenominator;
+                return res;
+            }
+        }
+
+        public static RationalNumber operator ++(RationalNumber n1)
+        {
+            n1.Numerator += 1;
+            return n1;
+        }
+
+        public static RationalNumber operator -(RationalNumber n1, RationalNumber n2)
+        {
+            RationalNumber res = new RationalNumber();
+            if (n1.Denominator == n2.Denominator)
+            {
+                res.Numerator = n1.Numerator - n2.Numerator;
+                res.Denominator = n1.Denominator;
+                return res;
+            }
+            else
+            {
+                int overallDenominator = n1.Denominator * n2.Denominator;
+                int temp1 = overallDenominator / n1.Denominator * n1.Numerator;
+                int temp2 = overallDenominator / n2.Denominator * n2.Numerator;
+                res.Numerator = temp1 - temp2;
+                res.Denominator = overallDenominator;
+                return res;
+            }
+        }
+
+        public static RationalNumber operator --(RationalNumber n1)
+        {
+            n1.Numerator -= 1;
+            return n1;
+        }
+
+        public static RationalNumber operator *(RationalNumber n1, RationalNumber n2)
+        {
+            RationalNumber res = new RationalNumber();
+            res.Numerator = n1.Numerator * n2.Numerator;
+            res.Denominator = n1.Denominator * n2.Denominator;
+            return res;
+        }
+
+        public static RationalNumber operator /(RationalNumber n1, RationalNumber n2)
+        {
+            RationalNumber res = new RationalNumber();
+            res.Numerator = n1.Numerator * n2.Denominator;
+            res.Denominator = n1.Denominator * n2.Numerator;
+            return res;
+        }
+
+        public static explicit operator float(RationalNumber rn)
+        {
+            return rn.Numerator / (float)rn.Denominator;
+        }
+
+        public static explicit operator int(RationalNumber rn)
+        {
+            return rn.Numerator / rn.Denominator;
+        }
+
+        public int Numerator
+        {
+            get
+            {
+                return _numerator;
+            }
+            set
+            {
+                _numerator = value;
+            }
+        }
+
+        public int Denominator
+        {
+            get
+            {
+                return _denominator;
+            }
+            set
+            {
+                _denominator = value;
+            }
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(obj == null || !this.GetType().Equals(obj.GetType()))
+            {
+                return false;
+            }
+            else
+            {
+                var number = (RationalNumber)obj;
+                return (Numerator == number.Numerator) && (Denominator == number.Denominator);
+            }
+        }
+
+        public override int GetHashCode()
+        {
+            return base.GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"{Numerator}/{Denominator}");
+        }
+    }
+}
