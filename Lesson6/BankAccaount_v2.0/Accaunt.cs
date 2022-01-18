774286ae0ba5163b4fe177a9475095be4101fdec
Tsimafei <tima.kutsko@gmail.com>
2022-01-18 23:11:32 +0300

    Часть1
    Обновленный банковский счет

---
+++ Lesson6/BankAccaount_v2.0/Accaunt.cs
@@ -0,0 +1,95 @@
+﻿using System;
+
+namespace BankAccaount_v2
+{
+    enum AccauntType
+    {
+        Budget,
+        ForeignCurrency,
+        Savings,
+    }
+
+    internal sealed class Accaunt
+    {
+        private int _id;
+        private int _balance;
+        private AccauntType _type;
+
+        public Accaunt() : this(0, AccauntType.Budget, false)
+        {
+        }
+
+        public Accaunt(int Balance) : this(Balance, AccauntType.Budget, false)
+        {
+        }
+
+        public Accaunt(AccauntType AccType) : this(0, AccType, false)
+        {
+        }
+
+        public Accaunt(int Balance, AccauntType AccType) : this(Balance, AccType, false)
+        {
+            this.GenerateId();
+            _balance = Balance;
+            _type = AccType;
+        }
+        
+        public Accaunt(int Balance, AccauntType AccType, bool isAutoId)
+        {
+            if (isAutoId)
+            {
+                this.GenerateId();
+            }
+            else
+            {
+                _id = -1;
+            }
+            _balance = Balance;
+            _type = AccType;
+        }
+
+        public void GenerateId()
+        {
+            Random rand = new Random();
+            int autoId = rand.Next(10000000, 99999999);
+            _id = autoId;
+        }
+
+        public static bool operator ==(Accaunt acc1, Accaunt acc2)
+        {
+             return (acc1._id == acc2._id && acc1._balance == acc2._balance && acc1._type == acc2._type);
+        }
+
+        public static bool operator !=(Accaunt acc1, Accaunt acc2)
+        {
+            return (acc1._id != acc2._id && acc1._balance != acc2._balance && acc1._type != acc2._type);
+        }
+
+        public override bool Equals(object obj)
+        {
+
+            if (ReferenceEquals(this, (Accaunt)obj))
+            {
+                return false;
+            }
+            return (this == (Accaunt)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            return (_id, _balance).GetHashCode();
+        }
+
+        public override string ToString()
+        {
+            if(_balance < 100)
+            {
+                return $"{_balance} - это не деньги. Забери карту и не позорься!";
+            }
+            else
+            {
+                return $"Id счета: {_id}, баланс {_balance}";
+            }
+        }
+    }
+}
---
+++ Lesson6/BankAccaount_v2.0/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace BankAccaount_v2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Accaunt acc1 = new Accaunt(1000, AccauntType.Savings, true);
+            Accaunt acc2 = new Accaunt(5000, AccauntType.Budget, true);
+            Accaunt acc3 = acc1;
+            Accaunt acc4 = new Accaunt(50, AccauntType.ForeignCurrency, false);
+            Accaunt acc5 = new Accaunt(50, AccauntType.ForeignCurrency, false);
+
+            bool isEqual12 = acc1 == acc2;
+            bool isEqual13 = acc1 == acc3;
+            bool isNotEqual12 = acc1 != acc2;
+            bool isEquals12 = acc1.Equals(acc2);
+            bool isEquals13 = acc1.Equals(acc3);
+            bool isEquals45 = acc4.Equals(acc5);
+            int hashCode1 = acc1.GetHashCode();
+            int hashCode4 = acc2.GetHashCode();
+            int hashCode5 = acc2.GetHashCode();
+            string str1 = acc1.ToString();
+            string str2 = acc5.ToString();
+
+
+            Console.ReadLine();
+        }
+    }
+}
